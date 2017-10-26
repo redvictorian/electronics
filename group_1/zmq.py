3fad9a87d181a8e0b7ef7311365af0a5e81460e2
Johan Mathe <johan.mathe@gmail.com>
2017-10-25 22:38:54 -0700

    add client

---
+++ group_1/zmq.py
@@ -0,0 +1,13 @@
+import zmq
+import random
+import sys
+import time
+
+port = "5556"
+if len(sys.argv) > 1:
+    port =  sys.argv[1]
+    int(port)
+
+context = zmq.Context()
+socket = context.socket(zmq.PUB)
+socket.bind("tcp://*:%s" % port)
